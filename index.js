32fa72101f9aab3a82a0f1e7b23b64bfc418f9b8
Justine Sermons <31023947+JustineSermons@users.noreply.github.com>
2024-01-29 20:15:06 -0500

    Created quote app

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+//quotes
 let quotes = ["Living is the art of getting used to what we didn't expect", "Never give up because great things take time", "Do good for others, it will come back in unexpected ways"]
-// Math.random(quotes)
+//creating an index variable to store the index number 
+//multiplying math.random() by 3 and rounding down to the nearest integer with math.floor to get an index from 0-2 to get any quote from the quotes array
 let index = Math.floor(Math.random() * 3)
+//creating a quote variable to store the array and random index number
 let quote = quotes[index]
+//displaying a random quote out of the three quotes to the console
 console.log(quote)
